67e38bc164603059893347c8f23df3abb4f61d2f
anakellyc <anakcampos@gmail.com>
2019-01-22 20:03:53 +0100

    website completed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require("express");
 var router = express.Router();
-
+const nodemailer = require("nodemailer");
+const config = require("../config");
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Ana's Portfolio" });
+});
+
+router.post("/contact", (req, res, next) => {
+  let { name, email, phone, message } = req.body;
+  console.log(req.body);
+  let transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: config.mailuser,
+      pass: config.pass
+    }
+  });
+  transporter
+    .sendMail({
+      from: `"Travel-Mate" <${config.mailuser}>`,
+      to: "anakcampos@gmail.com",
+      subject: `Website Contact Form: ${name}`,
+      text: message,
+      replyTo: email,
+      html: `<b>From: ${name}, phone: ${phone}</b>
+      <br>${message}</br>`
+    })
+    .then(info => {
+      res.status(200).send({ message: "message has been sent" });
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
